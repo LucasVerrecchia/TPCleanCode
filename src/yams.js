1f8f36693a690b3d5644a19faa59b3328ae2947e
Lucas Verrecchia <lverrecchia@myges.fr>
2025-10-30 10:18:26 +0100

    if des enfers pour gerer betement le carré + test

--- src/yams.js
+++ src/yams.js
@@ -1,7 +1,20 @@
-export function calculateBestScore(dices = [1, 1, 1, 1, 1]) {
+export function calculateBestScore(dices = [[1, 1, 1, 1, 1], [1, 2, 3, 5, 6]]) {
+
     if (!dices.length) {
         return 0;
     }
-    return 50;
+
+    if (dices[0][0] === dices[0][1] && dices[0][1] === dices[0][2] && dices[0][2] === dices[0][3] && dices[0][3] === dices[0][4]) {
+        return 50;
+    }
+
+    if (dices[0][0] === dices[0][1] && dices[0][1] === dices[0][2] && dices[0][2] === dices[0][3]
+        || dices[0][0] === dices[0][1] && dices[0][1] === dices[0][2] && dices[0][2] === dices[0][4]
+        || dices[0][0] === dices[0][1] && dices[0][1] === dices[0][3] && dices[0][3] === dices[0][4]
+        || dices[0][0] === dices[0][2] && dices[0][2] === dices[0][3] && dices[0][3] === dices[0][4]
+        || dices[0][1] === dices[0][2] && dices[0][2] === dices[0][3] && dices[0][3] === dices[0][4]
+    ) {
+        return 35;
+    }
 }
 
--- src/yams.test.js
+++ src/yams.test.js
@@ -9,6 +9,24 @@ describe("calculateBestScore without dices", () => {
 
 describe("calculateBestScore Yams (5 of a kind)", () => {
     it("should return 50 for [1, 1, 1, 1, 1]", () => {
-        expect(calculateBestScore([1, 1, 1, 1, 1])).toBe(50);
+        expect(calculateBestScore([[1, 1, 1, 1, 1], [1, 2, 3, 5, 6]])).toBe(50);
+    });
+});
+
+describe("calculateBestScore Carré (4 of a kind) with the 4 first same dices", () => {
+    it("should return sum of all dices for [2, 2, 2, 2, 3]", () => {
+        expect(calculateBestScore([[2, 2, 2, 2, 3], [1, 2, 3, 5, 6]])).toBe(35);
+    });
+});
+
+describe("calculateBestScore Carré (4 of a kind) with the last 4 same dices", () => {
+    it("should return sum of all dices for [2, 2, 2, 2, 3]", () => {
+        expect(calculateBestScore([[3, 2, 2, 2, 2], [1, 2, 3, 5, 6]])).toBe(35);
+    });
+});
+
+describe("calculateBestScore Carré (4 of a kind) with 4 random dices", () => {
+    it("should return sum of all dices for [2, 2, 2, 2, 3]", () => {
+        expect(calculateBestScore([[2, 2, 3, 2, 2], [1, 2, 3, 5, 6]])).toBe(35);
     });
 });
